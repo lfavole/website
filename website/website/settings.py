25c8f916a20b71aa6efb367cbb1d6f853508190a
Laurent FAVOLE <laurentfavole03@gmail.com>
2023-10-25 15:46:38 +0200

    Google login: set access_type to offline

--- website/website/settings.py
+++ website/website/settings.py
@@ -216,7 +216,7 @@ SOCIALACCOUNT_PROVIDERS = {
             "email",
         ],
         "AUTH_PARAMS": {
-            "access_type": "online",
+            "access_type": "offline",
         },
     },
     "github": {
