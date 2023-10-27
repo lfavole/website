508c97ceb291a20bef4c4cd00c42582fb682d8d6
lfavole <laurentfavole03@gmail.com>
2023-10-27 14:03:59 +0200

    Add Telegram login

--- website/website/settings.py
+++ website/website/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
     "allauth.socialaccount.providers.github",
+    "allauth.socialaccount.providers.telegram",
 ]
 SITE_ID = 1
 
@@ -226,6 +227,9 @@ SOCIALACCOUNT_PROVIDERS = {
         "SCOPE": [
             "user",
         ],
+    },
+    "telegram": {
+        "AUTH_PARAMS": {"auth_date_validity": 30},
     },
 }
 
