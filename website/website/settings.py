b879b25327b689185f95167dce2c1c9d1a5464a8
lfavole <88188568+lfavole@users.noreply.github.com>
2023-11-01 13:49:04 +0100

    Add offline compression

--- website/website/settings.py
+++ website/website/settings.py
@@ -274,6 +274,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     "compressor.finders.CompressorFinder",
 ]
+COMPRESS_OFFLINE = True
 COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 STATIC_ROOT = BASE_DIR / "static/"
 
