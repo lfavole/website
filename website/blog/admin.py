e07657dc7cd2fe2cd828c8e04c56a923c4077388
lfavole <88188568+lfavole@users.noreply.github.com>
2023-11-01 10:00:15 +0100

    Update permissions for files/images

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,4 @@
+from typing import Type
 from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
@@ -11,14 +12,38 @@ class DataInline(GenericTabularInline):
     Inline for files/images.
     """
 
+    model: Type[File] | Type[Image]
     readonly_fields = ("file",)
     extra = 0
 
-    def has_add_permission(self, *args, **kwargs):
-        return False
+    @property
+    def ma(self):
+        # ModelAdmin of the parent object
+        return admin.site._registry[self.parent_model]
 
-    def has_change_permission(self, *args, **kwargs):
-        return False
+    def has_add_permission(self, request, obj: File | Image | None):
+        if self.ma and (self.ma.has_add_permission(request) or self.ma.has_change_permission(request, obj)):
+            # we can add or change the parent object => we can add files/images
+            return True
+        return super().has_add_permission(request, obj)  # type: ignore
+
+    def has_change_permission(self, request, obj: File | Image):
+        if self.ma and self.ma.has_change_permission(request, obj):
+            # we can change the parent object => we can change files/images
+            return True
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj: File | Image):
+        if self.ma and self.ma.has_change_permission(request, obj):
+            # we can change the parent object => we can delete files/images
+            return True
+        return super().has_delete_permission(request, obj)
+
+    def has_view_permission(self, request, obj: File | Image):
+        if self.ma and self.ma.has_view_permission(request, obj):
+            # we can view the parent object => we can view files/images
+            return True
+        return super().has_view_permission(request, obj)
 
 
 class FilesInline(DataInline):
