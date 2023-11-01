79cf478103493fe9389f00f06f0b1b8580b0e15b
lfavole <88188568+lfavole@users.noreply.github.com>
2023-11-01 14:36:04 +0100

    Add own admin page for files and images

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,49 +1,75 @@
 from typing import Type
 from django import forms
 from django.contrib import admin
+from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from tinymce.widgets import AdminTinyMCE
 
 from .models import File, Image, Page
 
 
-class DataInline(GenericTabularInline):
+class DataMixin:
+    model: Type[File] | Type[Image]
+    fields = ("file",)
+
+    def get_modeladmin(self, obj):
+        if isinstance(self, InlineModelAdmin):
+            # ModelAdmin of the parent object
+            return admin.site._registry[self.parent_model]
+        return admin.site._registry[type(obj.item)] if obj else None
+
+    def has_add_permission(self, request, obj: File | Image | None = None):
+        ma = self.get_modeladmin(obj)
+        if ma and (ma.has_add_permission(request) or ma.has_change_permission(request, obj)):
+            # we can add or change the parent object => we can add files/images
+            return True
+        try:
+            return super().has_add_permission(request, obj)  # type: ignore
+        except TypeError:
+            return super().has_add_permission(request)  # type: ignore
+
+    def has_change_permission(self, request, obj: File | Image | None = None):
+        ma = self.get_modeladmin(obj)
+        if ma and ma.has_change_permission(request, obj):
+            # we can change the parent object => we can change files/images
+            return True
+        return super().has_change_permission(request, obj)  # type: ignore
+
+    def has_delete_permission(self, request, obj: File | Image | None = None):
+        ma = self.get_modeladmin(obj)
+        if ma and ma.has_change_permission(request, obj):
+            # we can change the parent object => we can delete files/images
+            return True
+        return super().has_delete_permission(request, obj)  # type: ignore
+
+    def has_view_permission(self, request, obj: File | Image | None = None):
+        ma = self.get_modeladmin(obj)
+        if ma and ma.has_view_permission(request, obj):
+            # we can view the parent object => we can view files/images
+            return True
+        return super().has_view_permission(request, obj)  # type: ignore
+
+
+class DataInline(DataMixin, GenericTabularInline):
     """
     Inline for files/images.
     """
-
-    model: Type[File] | Type[Image]
     readonly_fields = ("file",)
     extra = 0
 
-    @property
-    def ma(self):
-        # ModelAdmin of the parent object
-        return admin.site._registry[self.parent_model]
 
-    def has_add_permission(self, request, obj: File | Image | None):
-        if self.ma and (self.ma.has_add_permission(request) or self.ma.has_change_permission(request, obj)):
-            # we can add or change the parent object => we can add files/images
-            return True
-        return super().has_add_permission(request, obj)  # type: ignore
+@admin.register(File)
+class FilesAdmin(DataMixin, admin.ModelAdmin):
+    """
+    Admin interface for files.
+    """
 
-    def has_change_permission(self, request, obj: File | Image):
-        if self.ma and self.ma.has_change_permission(request, obj):
-            # we can change the parent object => we can change files/images
-            return True
-        return super().has_change_permission(request, obj)
 
-    def has_delete_permission(self, request, obj: File | Image):
-        if self.ma and self.ma.has_change_permission(request, obj):
-            # we can change the parent object => we can delete files/images
-            return True
-        return super().has_delete_permission(request, obj)
-
-    def has_view_permission(self, request, obj: File | Image):
-        if self.ma and self.ma.has_view_permission(request, obj):
-            # we can view the parent object => we can view files/images
-            return True
-        return super().has_view_permission(request, obj)
+@admin.register(Image)
+class ImagesAdmin(DataMixin, admin.ModelAdmin):
+    """
+    Admin interface for images.
+    """
 
 
 class FilesInline(DataInline):
