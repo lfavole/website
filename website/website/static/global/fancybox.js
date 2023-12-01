8729c967c9c24bd93df1d4169b717b3315b30a19
lfavole <88188568+lfavole@users.noreply.github.com>
2023-12-01 16:16:59 +0100

    Add Fancybox image lightbox

---
+++ website/website/static/global/fancybox.js
@@ -0,0 +1,32 @@
+$(function() {
+    function add_fancybox() {
+        $('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5/dist/fancybox/fancybox.css">')
+        .appendTo("head");
+        $.getScript("https://cdn.jsdelivr.net/npm/@fancyapps/ui@5/dist/fancybox/fancybox.umd.js")
+        .done(function() {
+            console.log($("img"));
+            Fancybox.bind("main img", {
+                "groupAll": true,
+                "Slideshow": {
+                    "timeout": 7000,
+                },
+                "Thumbnails": {
+                    "showOnStart": false,
+                },
+            });
+        });
+    }
+
+    if($("img").length) {
+        add_fancybox();
+    } else {
+        var $main = $("main")[0];
+        if($main) {
+            var mo = new MutationObserver(function() {
+                mo.disconnect();
+                add_fancybox();
+            });
+            mo.observe($main, {"childList": "img"});
+        }
+    }
+});
