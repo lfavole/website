9f1c59b4f355496e388384f2633c85b0d1d41d6b
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-07-10 12:54:21 +0200

    Fix emails not sent

--- website/settings.py
+++ website/settings.py
@@ -73,6 +73,7 @@ else:
 
 ADMINS = [item.split(":", 1) for item in os.environ.get("ADMINS", "").split(",")]
 
+DEFAULT_FROM_EMAIL = SERVER_EMAIL = os.environ.get("SERVER_EMAIL", "") or os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
