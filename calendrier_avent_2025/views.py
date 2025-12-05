31635c057c11f65a36db2dc643162b1d3038dd0e
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-12-05 18:08:11 +0100

    fix: Don't attempt fetching the day if too early

--- calendrier_avent_2025/views.py
+++ calendrier_avent_2025/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from django.http import Http404, HttpRequest
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from globals.models import Setting
 
 from website.views import has_permission
@@ -68,20 +68,11 @@ def day(request, day: int):
     today = get_today(request)
     day_date = date(YEAR, MONTH, day)
 
-    try:
-        if day == 25:
-            day_obj = Setting.objects.get(slug="25-12-2025")
-        else:
-            day_obj = Day.objects.get(day=day)
-    except Day.DoesNotExist:
-        day_obj = None  # hide the 404 error if too early (see below)
-
     if today < day_date and (not has_perm or not bool(request.GET.get("unlock"))):
         return render(
             request,
             "calendrier_avent_2025/too_early.html",
             {
-                "day_obj": day_obj,
                 "day": day,
                 "not_begun": today < start,
                 "date_to_wait": start if today < start else day_date,
@@ -89,8 +80,11 @@ def day(request, day: int):
             },
         )
 
-    if day_obj is None:
-        raise Http404
+    if day == 25:
+        day_obj = get_object_or_404(Setting, slug="25-12-2025")
+    else:
+        day_obj = get_object_or_404(Day, day=day)
+
     return render(
         request,
         "calendrier_avent_2025/day.html",
